b37632435ea81a06bcf31c205d1a6f9b409481d0
abhii371 <abhisheknale1709@gmail.com>
2024-04-11 21:42:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TradingViewWidget from "./component/TradingViewWidget";
+import "./App.css";
+function App() {
+  return (
+    <div style={{ width: "100%", height: "100vh", backgroundColor: "black" }}>
+      <TradingViewWidget />
+    </div>
+  );
+}
+
+export default App;
